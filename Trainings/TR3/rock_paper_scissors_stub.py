5ba40a4d62695a394f4db6c51229517ef57734e6
vaish-navi <vaishnavi.kcv@gmail.com>
2023-10-23 00:49:01 -0700

    Added training 3 files

---
+++ Trainings/TR3/rock_paper_scissors_stub.py
@@ -0,0 +1,54 @@
+# import random module
+import random
+
+# print multiline instruction
+# performstring concatenation of string
+print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
+      + "Rock vs Paper -> Paper wins \n"
+      + "Rock vs Scissors -> Rock wins \n"
+      + "Paper vs Scissors -> Scissor wins \n")
+
+while True:
+
+    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
+
+    # take the input from user
+
+
+
+    # OR is the short-circuit operator
+    # if any one of the condition is true
+    # then it return True value
+
+    # looping until user enter invalid input
+
+
+    # initialize value of choice_name variable
+    # corresponding to the choice value
+
+
+    # print user choice
+
+
+    # Computer chooses randomly any number
+    # among 1 , 2 and 3. Using randint method
+    # of random module
+
+
+    # looping until comp_choice value
+    # is equal to the choice value
+
+
+    # initialize value of comp_choice_name
+    # variable corresponding to the choice value
+
+    # we need to check of a draw
+
+    # condition for winning
+
+    # Printing either user or computer wins or draw
+
+
+# after coming out of the while loop
+# we print thanks for playing
+print("thanks for playing")
